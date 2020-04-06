15a9c6732edfc89da8ff0da1cb6016790f97e7e6
Pawel Swies <pav.swies@gmail.com>
2020-04-06 18:29:00 +0100

    adds collapsible styling and js to the netlify steps

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,18 @@
 const heading = document.querySelector('body');
+const collapsible = document.querySelector('.steps__collapsible');
+const collapsibleInner = document.querySelector('.steps__collapsible-inner');
+const collapsibleIcon = document.querySelector('.steps__collapsible-icon');
 
 function fadeIn() {
   heading.classList.add('animated');
 }
+
+function collapsibleOpen() {
+  collapsibleInner.classList.toggle('steps__collapsible-inner--active');
+  collapsibleIcon.classList.toggle('steps__collapsible-button--active')
+}
+
+collapsible.addEventListener('click', collapsibleOpen);
 
 window.onload = function animation() {
   this.fadeIn();
